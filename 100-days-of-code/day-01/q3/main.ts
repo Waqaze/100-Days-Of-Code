f34d8961e9e0160aa97392e3edb6e66eedbaaaad
Waqaze <153550986+Waqaze@users.noreply.github.com>
2024-06-27 13:02:48 -0700

    Add files via upload

--- 100-days-of-code/day-01/q3/main.ts
+++ 100-days-of-code/day-01/q3/main.ts
@@ -1,2 +1,7 @@
-let myName:string ="Waqas"
+let myName: string = "Waqas";
+
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());(
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
+
 
