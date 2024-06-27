3288ba29ba4eddd71800704a6a3a06f56309ac19
Waqaze <153550986+Waqaze@users.noreply.github.com>
2024-06-27 13:07:35 -0700

    Add files via upload

---
+++ 100-days-of-code/day 1/q2/main.js
@@ -0,0 +1,2 @@
+var myName = "Waqas";
+console.log("Hello ".concat(myName, ", would you like to learn some TypeScript today?"));
---
+++ 100-days-of-code/day 1/q3/main.ts
@@ -0,0 +1,7 @@
+let myName: string = "Waqas";
+
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());(
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
+
+
